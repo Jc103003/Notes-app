2d524ab0ca92791aaeb5ec2b575998c24b131e3a
James <james.c103003@gmail.com>
2025-06-19 21:50:04 -0700

    Added login form handling

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const loginForm = document.getElementById("login-form");
+
+//google sign in button
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
@@ -5,3 +8,10 @@ function onSignIn(googleUser) {
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
+  
+  loginForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const email = document.getElementById("login-email").value;
+    const password = document.getElementById("login-password").value; 
+    console.log(email, password);
+  })
