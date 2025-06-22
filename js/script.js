a579dd36d04b790c9e3b32ddc01eac723e15ab38
James Caldwell <james.c103003@gmail.com>
2025-06-22 16:45:24 -0700

    updated password to include Caps lock warning

--- js/script.js
+++ js/script.js
@@ -8,10 +8,19 @@ function onSignIn(googleUser) {
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
-  
-  formLogin.addEventListener("submit", function (e) {
+//taking inputs from email and password after submit
+formLogin.addEventListener("submit", function (e) {
     e.preventDefault();
     const email = document.getElementById("login-email").value;
     const password = document.getElementById("login-password").value; 
     console.log(email, password);
   })
+
+function checkCapsLock(event) {
+  const capsLockWarning = document.getElementById('caps-lock-warning')
+  if (event.getModifierState('CapsLock')) {
+    capsLockWarning.style.display = 'block';
+  } else {
+    capsLockWarning.style.display = 'none';
+  }
+}
